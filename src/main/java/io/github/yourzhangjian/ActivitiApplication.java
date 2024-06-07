dccc91358a66ee8794c71279d1590d83626b20cf
zdesign <1959018962@qq.com>
2024-06-08 02:22:52 +0800

    update

--- src/main/java/io/github/yourzhangjian/ActivitiApplication.java
+++ src/main/java/io/github/yourzhangjian/ActivitiApplication.java
@@ -1,7 +1,11 @@
 package io.github.yourzhangjian;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ActivitiApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(ActivitiApplication.class, args);
     }
 }
---
+++ src/main/java/io/github/yourzhangjian/service/HelloService.java
@@ -0,0 +1,12 @@
+package io.github.yourzhangjian.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloService {
+
+    public void sayHello() {
+        System.out.println("Hello, World! => 你好, 事件");
+    }
+
+}
